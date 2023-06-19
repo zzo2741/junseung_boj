68a4e8807ef9579bf1a3294fa797b0b96f16cbb4
zzo2741 <dlwnstmd@gmail.com>
2023-06-19 23:20:49 +0900

    아는 형님에 아는 동생에 아는 누나에 커밋이요~~

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,33 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String intput = br.readLine();
+        StringTokenizer st = new StringTokenizer(intput);
         Map<String, Integer> map = new HashMap<>();
         int x = 1;
-        char a = '1';
+        char a = '0';
         char b = 'A';
-        for(char i = a ; i < a + 9; i++){
+        for(char i = a ; i < a + 10; i++){
             map.put(String.valueOf(i), x++);
         }
         for(char i = b ; i < b + 26; i++){
             map.put(String.valueOf(i), x++);
         }
-        System.out.println(map.toString());
-        char[] list = st.nextToken().toCharArray();
-        int total = 1;
-        for(int i = 0; i < list.length;i++){
-            total *= map.get(String.valueOf(list[i]));
+        StringBuilder sb = new StringBuilder();
+        sb.append(st.nextToken());
+        String str = String.valueOf(sb.reverse());
+        char[] list = str.toCharArray();
+        int y = Integer.parseInt(st.nextToken());
+        int total = 0;
+        int temp = 0;
+        for(int i = 0; i < list.length; i++){
+            total = map.get(String.valueOf(list[i]));
+            for(int j = 1; j <= i; j++){
+
+            }
+            total = total + (map.get(String.valueOf(list[i])) * temp);
+            temp = 1;
         }
-        System.out.println(total - 1);
-
+        System.out.print(total);
     }
-
 }
